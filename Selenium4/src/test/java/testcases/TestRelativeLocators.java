af48f5cdd871faf5cfe68d6cfcf1600e53326937
Asavari Dudhe <dudhe.asavari@gmail.com>
2021-07-09 20:27:02 +0530

    First

--- Selenium4/src/test/java/testcases/TestRelativeLocators.java
+++ Selenium4/src/test/java/testcases/TestRelativeLocators.java
@@ -14,6 +14,8 @@ public class TestRelativeLocators {
 	public static void main(String[] args) {
 		
 		
+		
+		
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
